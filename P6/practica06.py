5f72c06e548cb17892a0cabf4c5e802f7b8dd051
LauraWangQiu <yiwang03@ucm.es>
2024-12-24 12:22:45 +0100

    P6 Ejercicio 2-3

--- P6/practica06.py
+++ P6/practica06.py
@@ -1,35 +1,55 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from Utils import load_data_csv
-from sklearn.metrics import silhouette_score
-from sklearn.cluster import KMeans
-from kneed import KneeLocator
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 # Ejercicio 2
-np.random.seed(0)
+x_columns = ["ray1", "ray2", "ray3", "ray4", "ray5", "kartx", "karty", "kartz", "time"]
+gameData, X, y = load_data_csv("KartData.csv", x_columns, "action")
+#display(gameData) # Para el notebook
 
-data, X, y = load_data_csv("KartData.csv", ["ray1", "ray2", "ray3", "ray4", "ray5", "kartx", "karty", "kartz", "time"], "action")
+df1 = pd.DataFrame(gameData, columns=x_columns)
 
-# silhouette_coefficients = []
-inertia_values = []
-for k in range(2, 9):
-    kkkmeans = KMeans(n_clusters=k)
-    kkkmeans.fit(X)
-    # score = silhouette_score(X, kkkmeans.labels_)
-    # silhouette_coefficients.append(score)
-    inertia_values.append(kkkmeans.inertia_)
+scaling = StandardScaler()
+scaling.fit(df1)
+scaled_data = scaling.transform(df1)
 
-fig, ax = plt.subplots(figsize = (24, 7))
-# ax.scatter(range(2, 9), silhouette_coefficients)
-ax.scatter(range(2, 9), inertia_values)
-ax.set_xticks(range(2, 9))
-ax.set_xlabel("Número de clusters")
-# ax.set_ylabel("Promedio coeficientes de Silhouette")
-ax.set_ylabel("SSE / Valores de inercia")
+principal = PCA(n_components=3)
+principal.fit(scaled_data)
+x = principal.transform(scaled_data)
 
-kl = KneeLocator(range(2, 9), inertia_values, curve="convex", direction="decreasing")
-print(kl.knee)
+feature_weights_mapping = {}
+for i, component in enumerate(principal.components_):
+    component_feature_weights = zip(x_columns, component)
+    sorted_feature_weight = sorted(component_feature_weights, key=lambda x: abs(x[1]), reverse=True)
+    feature_weights_mapping[i] = sorted_feature_weight
 
-plt.grid()
+# En nuestro dataset, el número de clases es 3
+# porque en ningún momento deceleramos (BRAKE)
+label1 = feature_weights_mapping[0][0][0]
+label2 = feature_weights_mapping[1][0][0]
+label3 = feature_weights_mapping[2][0][0]
+
+left_accelerate_pc1 = gameData[label1][y == "LEFT_ACCELERATE"]
+right_accelerate_pc1 = gameData[label1][y == "RIGHT_ACCELERATE"]
+accelerate_pc1 = gameData[label1][y == "ACCELERATE"]
+left_accelerate_pc2 = gameData[label2][y == "LEFT_ACCELERATE"]
+right_accelerate_pc2 = gameData[label2][y == "RIGHT_ACCELERATE"]
+accelerate_pc2 = gameData[label2][y == "ACCELERATE"]
+left_accelerate_pc3 = gameData[label3][y == "LEFT_ACCELERATE"]
+right_accelerate_pc3 = gameData[label3][y == "RIGHT_ACCELERATE"]
+accelerate_pc3 = gameData[label3][y == "ACCELERATE"]
+
+fig = plt.figure(figsize=(12,10))
+ax = fig.add_subplot(projection='3d')
+ax.scatter(left_accelerate_pc1, left_accelerate_pc2, left_accelerate_pc3, c='red', marker='X', s=15)
+ax.scatter(right_accelerate_pc1, right_accelerate_pc2, right_accelerate_pc3, c='green', marker='o', s=15)
+ax.scatter(accelerate_pc1, accelerate_pc2, accelerate_pc3, c='blue', marker='*', s=15)
+ax.set_xlabel(label1)
+ax.set_ylabel(label2)
+ax.set_zlabel(label3)
+
 plt.savefig("ejercicio2.png")
 plt.show()
