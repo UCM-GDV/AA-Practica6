59d32c0827c448885851c8cab78599ab7539d009
LauraWangQiu <yiwang03@ucm.es>
2024-12-27 16:02:08 +0100

    Cositas: normalizacion de los datos antes del split y mapeado de y_pred
    Falta clasificación

--- P6/practica06.py
+++ P6/practica06.py
@@ -1,13 +1,32 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from Utils import load_data_csv,one_hot_encoding,accuracy
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-import MLP_Complete
 from sklearn.neural_network import MLPClassifier
- 
+from Utils import load_data_csv,one_hot_encoding,accuracy
+from MLP_Complete import MLP_Complete
+
+class Labels:
+    NONE = 0
+    ACCELERATE = 1
+    BRAKE = 2
+    LEFT_ACCELERATE = 3
+    RIGHT_ACCELERATE = 4
+    LEFT_BRAKE = 5
+    RIGHT_BRAKE = 6
+
+label_mapping = {
+    0: "NONE",
+    1: "ACCELERATE",
+    2: "BRAKE",
+    3: "LEFT_ACCELERATE",
+    4: "RIGHT_ACCELERATE",
+    5: "LEFT_BRAKE",
+    6: "RIGHT_BRAKE"
+}
+
 # Ejercicio 2
 x_columns = ["ray1", "ray2", "ray3", "ray4", "ray5", "kartx", "karty", "kartz", "time"]
 gameData, X, y = load_data_csv("KartData.csv", x_columns, "action")
@@ -57,30 +76,35 @@ ax.set_zlabel(label3)
 plt.savefig("ejercicio2.png")
 plt.show()
 
-#Ejercicio 4 
-## Split de los datos 
+# Ejercicio 4 
 x_columns = ["ray1", "ray2", "ray3", "ray4", "ray5", "kartx", "karty", "kartz", "time"]
 kartData, X, y = load_data_csv("KartData.csv", x_columns, "action")
-X_train, X_test, y_train, y_test = train_test_split(X.T, y, random_state=0, test_size=0.33)
+
+# Normalizar los datos
+scaler = StandardScaler()
+X = scaler.fit_transform(X).T
+
+# Split de los datos 
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.33)
 yEnc = one_hot_encoding(np.array(y_train))
 
-#Perceptron multicapa de implementacion propia 
-lr = 0.001
-mlpc = MLP_Complete.MLP_Complete(X_train.shape[1],[9],yEnc.shape[1])
-mlpc.backpropagation(X_train,yEnc,0.0,lr,2000)
-a1,ai,zi = mlpc.feedforward(X_test)
-pred = mlpc.predict(ai[len(ai) - 1])
-accuracy_MLPC = accuracy(y_test, pred)
-print (" MLPC ac " + str( accuracy_MLPC))
-#Perceptron multicapa de SKlearn
+alpha_ = 1.0
+lambda_ = 0.0
 
-mlp = MLPClassifier(activation='logistic', alpha=0.0, learning_rate='constant', learning_rate_init= lr, max_iter=2000, random_state= 0, epsilon= 0.12)
-mlp.fit(X_train, y_train)
+# Perceptron multicapa de implementacion propia 
+mlpc = MLP_Complete(X_train.shape[1],[9],yEnc.shape[1])
+Jhistory = mlpc.backpropagation(X_train,yEnc,alpha_,lambda_,2000)
+a1,ai,zi = mlpc.feedforward(X_test)
+y_pred_mlp = mlpc.predict(ai[len(ai) - 1])
+y_pred_mlp_labels = [label_mapping[label] for label in y_pred_mlp]
+
+# Perceptron multicapa de SKlearn
+mlp = MLPClassifier(activation='logistic',alpha=lambda_,learning_rate='constant',learning_rate_init=alpha_,max_iter=2000,random_state=0,epsilon=0.12)
+mlp.fit(X_train,y_train)
 y_pred_sklearn = mlp.predict(X_test)
 
-
-#Comprobacion de resultados
-accuracy_sklearn = accuracy(y_test, y_pred_sklearn)
-
-print ("SK ac " + str( accuracy_sklearn))
-
+# Comprobacion de resultados
+accuracy_mlpc = accuracy(y_test,y_pred_mlp_labels)
+print("MLP accuracy: " + str(accuracy_mlpc))
+accuracy_sklearn = accuracy(y_test,y_pred_sklearn)
+print("SKlearn accuracy: " + str(accuracy_sklearn))
